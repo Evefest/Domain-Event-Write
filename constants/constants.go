88a208272da20d9574668d241ecafb8eb87b2c9e
jaidernperez <jaidernico408@egmail.com>
2024-01-22 15:42:15 -0500

    feat: add base files

---
+++ constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+const WelcomeMessage = "Welcome API event Write"
+const BadRequest = "Bad request"
+const InternalServerError = "Internal server error"
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jaidernperez/Domain-Event-Write
+
+go 1.21.5
